2c66b269e72e96a51bd6b2cba959f9eab1e93f40
Waasiq Masood <waasiqmasood@gmail.com>
2022-01-02 15:29:36 +0100

    Post component added

--- fb-clone/src/Components/MessageSender.js
+++ fb-clone/src/Components/MessageSender.js
@@ -1,10 +1,70 @@
-import React from 'react'
+import React , { useState } from 'react'
 import '../css/MessageSender.css';
 
+// Material UI Icons
+import { Avatar } from '@mui/material';
+import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+
+
 function MessageSender() {
+    const [input, setInput] = useState('');
+    const [imageURL, setImageURL] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Database stuff
+
+        setInput("");
+        setImageURL("");
+    };
+
     return (
         <div className='messageSender'>
+            <div className="messageSender_top">
+                <Avatar />
+                <form action="">
+                    <input 
+                        value = { input }
+                        onChange = {(e) =>  setInput(e.target.value) }
+                        className = 'messageSender_input'
+                        type="text"
+                        placeholder={'Whats on your mind'}
+                    />
 
+                    <input 
+                        value = {imageURL}
+                        onChange = { (e) => setImageURL(e.target.value) }
+                        placeholder="Image URL (Optional)"
+                    />
+                
+                    <button onClick={handleSubmit} type='submit'>
+                        Hidden Submit
+                    </button>
+                </form>
+            </div>
+
+            <div className="messageSender_bottom">
+                <div className="messageSender_option">
+                    <VideoCameraFrontIcon style={{ color: 'red' }} />
+                    <h3>Live video</h3>
+                </div>
+
+                <div className="messageSender_option">
+                    <PhotoLibraryIcon style={{ color: "green" }} />
+                    <h3>Photo/video</h3>
+                </div>
+
+                <div className="messageSender_option">
+                    <EmojiEmotionsIcon style={{ color: "orange" }} />
+                    <h3>Feeling/activity</h3>
+                </div>
+              
+                
+               
+            </div>
             
         </div>
     )
---
+++ fb-clone/src/Components/Post.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../css/Post.css';
+
+// Material UI Icons
+import { Avatar } from '@mui/material';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+
+function Post( {profilePic, image, username, timestamp, message }) {
+    return (
+        <div className='post'>
+                <div className="post_top">
+                    <Avatar 
+                        src={profilePic} 
+                        className = 'post_avatar'
+                    />
+                    <div className="post_topInfo">
+                        <h3>{ username }</h3>
+                        <p>{ timestamp }</p>
+                    </div>
+                </div>
+
+
+                {/* Main Message of the post */}
+                <div className="post_bottom">
+                    <p>{ message }</p>
+                </div>
+
+                <div className="post_image">
+                    <img src={image} alt="" />
+                </div>
+
+                <div className="post_options">
+                    <div className="post_option">
+                        <ThumbUpAltOutlinedIcon />
+                        <p>Like</p>
+                    </div>
+                    <div className="post_option">
+                        <ChatBubbleOutlineOutlinedIcon />
+                        <p>Comment</p>
+                    </div>
+                    <div className="post_option">
+                        <ShareOutlinedIcon />
+                        <p>Share</p>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Post
